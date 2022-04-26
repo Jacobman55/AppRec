054d528dff70912090c232c93d147024f5ca04cd
RjacoboR <92778805+Jacobman55@users.noreply.github.com>
2022-04-26 10:10:59 +0200

    Sprint 4
    Comenzado con el List y creada la Request para visualizar los datos del json.

--- app/src/main/java/com/afundacionfp/apprec/OrderListActivity.java
+++ app/src/main/java/com/afundacionfp/apprec/OrderListActivity.java
@@ -2,13 +2,66 @@ package com.afundacionfp.apprec;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class OrderListActivity extends AppCompatActivity {
+
+    private JSONObject nombre;
+    private JSONObject alltheData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_list);
+        JsonArrayRequest request = new JsonArrayRequest(
+                Request.Method.GET,
+                "https://625e5cdf873d6798e2a5dce7.mockapi.io/api/vf1/Pedido",
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        List<OrderDto> alltheData = new ArrayList<>();
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+                                JsonObject pedido = response.getJSONObject(i);
+                                OrderDto data = new OrderDto(nombre);
+                                alltheData.add(nombre);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            };
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Context context = getApplicationContext();
+                        int duration = Toast.LENGTH_SHORT;
+                        CharSequence text ="Fallo";
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+                    }
+                }
+
+        );
+        RequestQueue cola = Volley.newRequestQueue(this);
+        cola.add(request);
     }
 }
